f80cf49db4e6a4acf6fa24a89cbc22b31bad85f8
copelandt2032-a11y <copelandt2032@students.lee.k12.al.us>
2026-07-14 16:17:05 -0500

    Create Home component with PageWrapper

---
+++ home.jsx
@@ -0,0 +1,12 @@
+import PageWrapper from "../components/PageWrapper";
+
+export default function Home() {
+  return (
+    <PageWrapper>
+      <section className="hero">
+        <h1>Hi, I'm [Your Name]</h1>
+        <p>Creative Developer & 3D Web Designer</p>
+      </section>
+    </PageWrapper>
+  );
+}
